811da86a1346ef5f9a1ee41251f0821d820afcfa
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-16 21:30:59 -0300

    WIP: adding markups

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,41 @@
 class Utils {
-    static async mapFields(fields) {
-      // TODO => ADD VALIDATION
-      return fields
-        .map(item => item.split(':'))
-        .reduce((acc, item) => {
-          const formated = item.map(elem =>
-            elem.includes('_') ? elem.replace(/_/, ' ').toUpperCase() : elem.toUpperCase()
-          );
-          acc.push(formated);
-          return acc;
-        }, []);
-    }
-  
-    static async buildQuery(mappedFields) {
-      return mappedFields.reduce((acc, item, idx, self) => {
-        acc += `${item.join(' ')} ${(item[3] === 'PRIMARY KEY' && 'DEFAULT gen_random_uuid()') ||
-          ''}${self.length - 1 === idx ? '' : ','}`;
+  static async mapFields(fields) {
+    // TODO => ADD VALIDATION
+    return fields
+      .map(item => item.split(':'))
+      .reduce((acc, item) => {
+        const formated = item.map(elem =>
+          elem.includes('_') ? elem.replace(/_/, ' ').toUpperCase() : elem.toUpperCase()
+        );
+        acc.push(formated);
         return acc;
-      }, '');
-    }
-  
-    static async buildTableQuery(tableName, query) {
-      return `CREATE TABLE ${tableName} (${query.trim()});`;
-    }
+      }, []);
   }
-  
-  module.exports = Utils;
-  
+
+  static async buildQuery(mappedFields) {
+    return mappedFields.reduce((acc, item, idx, self) => {
+      acc += `${item.join(' ')} ${(item[3] === 'PRIMARY KEY' && 'DEFAULT gen_random_uuid()') ||
+        ''}${self.length - 1 === idx ? '' : ','}`;
+      return acc;
+    }, '');
+  }
+
+  static async buildMarkups() {
+    const up = `--- UP`;
+    const down = `--- DOWN`;
+    return { up, down };
+  }
+
+  static async buildTableQuery(tableName, query) {
+    const markups = await Utils.buildMarkups();
+
+    const up = `CREATE TABLE ${tableName} (${query.trim()});`;
+    const down = `DROP TABLE ${tableName};`;
+
+    const result = `${markups.up}\n${up}\n${markups.down}\n${down}`;
+
+    return result;
+  }
+}
+
+module.exports = Utils;
